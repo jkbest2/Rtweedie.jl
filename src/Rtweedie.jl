37530c7d87cf1b11dfc69a7404fa76eab8ac7fa1
John Best <jkbest@gmail.com>
2018-02-11 14:09:58 -0800

    Implement Tweedie type and rand method.

--- src/Rtweedie.jl
+++ src/Rtweedie.jl
@@ -1,5 +1,33 @@
 module Rtweedie
 
-# package code goes here
+using RCall
+
+import Base.rand
+
+export Tweedie,
+   rand
+
+reval("library(tweedie)")
+
+"""
+    Tweedie{T}
+       μ::T
+       ξ::T
+       ϕ::T
+
+Tweedie distribution. Typically used with 1 < ξ < 2 to give compound
+gamma-Poisson. Has positive mass at zero, and is continuous for positive
+Reals.
+"""
+struct Tweedie{T}
+    μ::T
+    ξ::T
+    ϕ::T
+end
+
+function rand(Tw::Tweedie, n::Integer = 1)
+rtweed = reval("rtweedie($n, xi = $(Tw.ξ), mu = $(Tw.μ), phi = $(Tw.ϕ))")
+    rcopy(rtweed)
+end
 
 end # module
